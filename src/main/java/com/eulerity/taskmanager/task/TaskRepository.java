3e93f4b1f5b360235499899a735e3815f3790518
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-06-03 13:05:19 -0400

    feat(task): add derived queries for active-HIGH count and FIFO victim

--- src/main/java/com/eulerity/taskmanager/task/TaskRepository.java
+++ src/main/java/com/eulerity/taskmanager/task/TaskRepository.java
@@ -2,5 +2,13 @@ package com.eulerity.taskmanager.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    long countByPriorityAndStatusIn(Priority priority, Collection<Status> statuses);
+
+    Optional<Task> findFirstByPriorityAndStatusInOrderByCreatedAtAsc(
+            Priority priority, Collection<Status> statuses);
 }
